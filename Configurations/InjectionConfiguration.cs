b21d42520d0c70a973cf04e26f4b534f8169a0bc
yudek <nbyauio@gmail.com>
2025-06-28 01:12:53 +0200

    DIC made for bot

---
+++ Configurations/InjectionConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TrendingDiscordBot.Services;
+
+namespace TrendingDiscordBot.Configurations;
+
+public class InjectionConfiguration
+{
+    public static async Task<IServiceProvider> CreateProvider()
+    {
+        var collection = new ServiceCollection()
+            .AddSingleton(await GetDiscordBot())
+            .AddSingleton<CommandService>()
+            .AddSingleton<LoggingService>();
+
+        return collection.BuildServiceProvider();
+    }
+
+    private static async Task<DiscordSocketClient> GetDiscordBot()
+    {
+        var bot = new DiscordSocketClient();
+        var builder = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        var token = builder["DiscordTestingBotKey"];
+
+        await bot.LoginAsync(TokenType.Bot, token);
+        await bot.StartAsync();
+
+        return bot;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TrendingDiscordBot.Configurations;
+using TrendingDiscordBot.Services;
+
+namespace TrendingDiscordBot;
+
+internal class Program
+{
+    private static IServiceProvider _serviceProvider;
+
+    private static async Task Main(string[] args)
+    {
+        _serviceProvider = await InjectionConfiguration.CreateProvider();
+        
+        _serviceProvider.GetRequiredService<LoggingService>();
+
+        await Task.Delay(-1);
+    }
+}
