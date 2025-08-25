cc6b8a2e678f737a827da2e9b7f4f6d7d01de3da
yudek <nbyauio@gmail.com>
2025-08-26 01:02:33 +0200

    added threshold to cfg

--- Modules/ForwardModule.cs
+++ Modules/ForwardModule.cs
@@ -6,43 +6,40 @@ namespace TrendingDiscordBot.Modules;
 
 public class ForwardModule
 {
-    private const int Threshold = 4;
     private readonly ulong _channel;
-    private readonly ulong _server;
     private readonly DiscordSocketClient _client;
-        
-    public ForwardModule(DiscordSocketClient client)
+    private readonly ulong _server;
+    private readonly int _threshold;
+
+    public ForwardModule(DiscordSocketClient client, IConfigurationRoot config)
     {
         _client = client;
-        
-        var config = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
-            .Build();
-
         _server = Convert.ToUInt64(config["ServerID"]);
         _channel = Convert.ToUInt64(config["ForwardChannelID"]);
+        _threshold = Convert.ToInt32(config["Threshold"]);
+        
+        Console.WriteLine($"ForwardModule initalized with threshold: {_threshold}");
     }
 
     public async Task<bool> HandleMessage(SocketMessage msg)
     {
-        Console.WriteLine("Checking message: " + msg.Id + " " + msg.Content + " : " + msg.Reactions.Count);
+        //Console.WriteLine("Checking message: " + msg.Id + " " + msg.Content + " : " + msg.Reactions.Count);
 
-        if (msg.Reactions.Count < Threshold) return false;
-        
+        if (msg.Reactions.Count < _threshold) return false;
+
         var uniqueUsers = new HashSet<ulong>();
 
         foreach (var reaction in msg.Reactions)
         {
             var emoji = reaction.Key;
-            
+
             var users = await msg.GetReactionUsersAsync(emoji, int.MaxValue).FlattenAsync();
 
             foreach (var user in users)
                 uniqueUsers.Add(user.Id);
         }
 
-        if (uniqueUsers.Count < Threshold) return false;
+        if (uniqueUsers.Count < _threshold) return false;
 
         if (_client.GetChannel(_channel) is IMessageChannel destinationChannel)
         {
