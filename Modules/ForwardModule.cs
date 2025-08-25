d651405d56940ffbfa708bffbfa4b27aff9ba664
yudek <nbyauio@gmail.com>
2025-08-26 00:08:38 +0200

    added unique user checking

--- Modules/ForwardModule.cs
+++ Modules/ForwardModule.cs
@@ -3,25 +3,47 @@ using Discord.WebSocket;
 
 namespace TrendingDiscordBot.Modules;
 
-public class ForwardModule
+public class ForwardModule(
+    DiscordSocketClient client
+)
 {
     private const int Threshold = 4;
     private const ulong Channel = 1388552600646848673;
-    
-    private readonly DiscordSocketClient _client;
+    private const ulong Server = 1051185027054051378;
 
-    public ForwardModule(DiscordSocketClient client)
+    public async Task<bool> HandleMessage(SocketMessage msg)
     {
-        _client = client;
-    }
+        Console.WriteLine("Checking message: " + msg.Id + " " + msg.Content + " : " + msg.Reactions.Count);
 
-    public async Task HandleMessage(SocketMessage msg)
-    {
-        //if (msg.Reactions.Count < Threshold) return; //TODO cache check
+        if (msg.Reactions.Count < Threshold) return false;
+        
+        var uniqueUsers = new HashSet<ulong>();
 
-        if (_client.GetChannel(Channel) is IMessageChannel destinationChannel)
+        foreach (var reaction in msg.Reactions)
         {
-            await destinationChannel.SendMessageAsync(msg.Content); //TODO implement compiling image or a forward mimicking
+            var emoji = reaction.Key;
+
+            // this returns an async enumerable of users
+            var users = await msg.GetReactionUsersAsync(emoji, int.MaxValue).FlattenAsync();
+
+            foreach (var user in users)
+                uniqueUsers.Add(user.Id);
         }
+
+        if (uniqueUsers.Count < Threshold) return false;
+
+        if (client.GetChannel(Channel) is IMessageChannel destinationChannel)
+        {
+            await destinationChannel
+                .SendMessageAsync(
+                    $"{msg.Author.Username}({msg.Author.GlobalName}): {msg.Content}",
+                    embeds: msg.Embeds.ToArray(),
+                    stickers: msg.Stickers.ToArray<ISticker>()
+                );
+            await destinationChannel
+                .SendMessageAsync($"https://discord.com/channels/{Server}/{msg.Channel.Id}/{msg.Id}");
+        }
+
+        return true;
     }
 }
