f7855d5533e0bea8c6db6a740128bfd2da76fde0
yudek <nbyauio@gmail.com>
2025-10-18 01:17:26 +0200

    added caching and logging

---
+++ Configurations/FromJsonMemCacheEntryOptions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace TrendingDiscordBot.Configurations;
+
+public class FromJsonMemCacheEntryOptions : MemoryCacheEntryOptions
+{
+    public FromJsonMemCacheEntryOptions(int timeout)
+    : base()
+    {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(timeout);
+    }
+}
--- Configurations/InjectionConfiguration.cs
+++ Configurations/InjectionConfiguration.cs
@@ -1,8 +1,10 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using TrendingDiscordBot.Modules;
 using TrendingDiscordBot.Services;
 
@@ -10,6 +12,8 @@ namespace TrendingDiscordBot.Configurations;
 
 public static class InjectionConfiguration
 {
+    public static ulong ServerId { get; private set; }
+    
     public static async Task<IServiceProvider> CreateProvider()
     {
         var config = new DiscordSocketConfig
@@ -26,19 +30,33 @@ public static class InjectionConfiguration
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("configuration.json", false, true)
             .Build();
+        
+        ServerId = Convert.ToUInt64(configurationRoot["ServerID"]);
 
         var bot = await GetDiscordBot(config, configurationRoot);
+        var options = new FromJsonMemCacheEntryOptions(Convert.ToInt32(configurationRoot["Timeout"]));
+        
+        var logLevel = configurationRoot["Logging:LogLevel"] ?? "Information";
 
         var services = new ServiceCollection()
-            .AddLogging()
+            .AddLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.AddConsole();
+                builder.SetMinimumLevel(Enum.Parse<LogLevel>(logLevel, true));
+            })
+            .AddMemoryCache()
             .AddSingleton<IConfigurationRoot>(configurationRoot)
             .AddSingleton<DiscordSocketClient>(bot)
+            .AddSingleton<MemoryCacheEntryOptions>(options)
             .AddSingleton<ForwardModule>()
             .AddSingleton<CommandService>()
             .AddSingleton<LoggingService>()
             .AddSingleton<CommandHandler>()
             .AddSingleton<ForwardInterface>()
             .BuildServiceProvider();
+
+        Console.WriteLine($"Logging level: {logLevel}");
 
         return services;
     }
